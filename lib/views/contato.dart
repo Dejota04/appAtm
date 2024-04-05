89418bb2a532a9990438b200a8c42eb3f4bb5cd5
Dejota04 <dssantana04@gmail.com>
2024-04-04 21:46:30 -0300

    add farofa

--- lib/views/contato.dart
+++ lib/views/contato.dart
@@ -18,7 +18,8 @@ class _ContatoState extends State<Contato> {
             Image.asset("imagens/detalhe_contato.png"),
             Text("Sobre contato")
           ],
-        )
+        ),
+         Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae risus consequat, pretium velit quis, tristique purus. Morbi lacinia, eros sit amet tincidunt aliquam, felis nibh ultrices nisl, nec pellentesque augue turpis luctus risus. Ut hendrerit, erat ut ullamcorper dapibus, mi massa mattis ante, vitae consectetur orci purus nec nulla. Vivamus placerat diam quis rhoncus ultricies. Etiam semper lacus vel ultrices laoreet. Nullam sed fringilla massa, blandit consequat enim. Sed venenatis purus at ligula convallis elementum. Sed faucibus, sapien vel porttitor tempus, quam nunc iaculis diam, id maximus diam sem quis leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque eu magna in ex feugiat laoreet.")
       ]),
     );
   }
